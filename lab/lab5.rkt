0a8d995cde2e562112b0a1d463df985c8f5b1813
JJ Lumagbas <jjlumagbas@gmail.com>
2019-02-18 15:06:13 -0600

    Add lab5 and project 2

---
+++ lab/lab5.rkt
@@ -0,0 +1,57 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname lab5) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+(require 2htdp/image)
+
+
+; Student ID 1:
+; Student ID 2:
+
+#|
+
+Design the following functions, following the design recipe:
+
+0. Read problem statement, write wishlist
+1. Data definitions
+2. Signature, purpose, stub
+3. Examples
+4. Template
+5. Function body
+6. Test and debug
+
+Note 1: Keep the following in your submission, labelled and commented out:
+
+- Stub
+- Template
+
+Note 2: To get full points, include a full set of examples (tests) to
+prove that the function works correctly for any possible (valid) inputs
+
+Note 3: Have 1 person do Part 1, then hand over keyboard/mouse to the
+ second person for Part 2
+
+|#
+
+
+; Part 1: Design the function bump-up, which accepts a letter grade
+; and returns the next highest letter grade
+
+
+
+
+
+
+
+; Part 2: Design a function spectrum-color which returns a square with the given
+;    color of visible light, given the measure of the wavelength.
+;    The square should be solid and be 60 pixels on one side
+;
+; Refer to: http://gsp.humboldt.edu/OLM/Courses/GSP_216_Online/images/visible-table.png
+;  (If a number appears in two ranges, include it in the lower range)
+
+
+
+
+
+
+
