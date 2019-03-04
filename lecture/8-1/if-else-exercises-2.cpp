35bc35f5852cadfdbf007ab7239237a78d8d52c9
JJ Lumagbas <jjlumagbas@gmail.com>
2019-03-04 10:23:43 -0600

    More if-else exercises

---
+++ lecture/8-1/if-else-exercises-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ph(double p) {
+  if () {
+
+  } else if () {
+
+  } else {
+
+  }
+}
+
+// (8 <= p) and (p <= 14)
+// (0 <= p) and (p <= 6)
+
+// return "acid";
+// return "neutral";
+// return "base";
+
+int main() {
+  cout << boolalpha;
+
+  
+
+  return EXIT_SUCCESS;
+}
