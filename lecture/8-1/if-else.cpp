8a4df7b8765a85fc0a4392ab5c3fb12e3b52312d
JJ Lumagbas <jjlumagbas@gmail.com>
2019-03-04 10:17:16 -0600

    Reformatted code for C++

--- lecture/8-1/if-else.cpp
+++ lecture/8-1/if-else.cpp
@@ -4,11 +4,17 @@ using namespace std;
 
 char letter_grade(double grade) {
 
-  if (grade >= 90) { return 'A'; }
-  else if (grade >= 80) { return 'B'; }
-  else if (grade >= 70) { return 'C'; }
-  else if (grade >= 60) { return 'D'; }
-  else { return 'F'; }
+  if (grade >= 90) { 
+    return 'A'; 
+  } else if (grade >= 80) { 
+    return 'B'; 
+  } else if (grade >= 70) { 
+    return 'C'; 
+  } else if (grade >= 60) { 
+    return 'D'; 
+  } else { 
+    return 'F'; 
+  }
 
 }
 
