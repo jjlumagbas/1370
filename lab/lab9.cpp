b3a21343b037dea837e6199ce8a6fbc37751d6a0
JJ Lumagbas <jjlumagbas@gmail.com>
2019-03-22 10:40:28 -0500

    Add lab 9 and project 3-2

---
+++ lab/lab9.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Student ID 1:
+// Student ID 2:
+
+// For each of the functions below, write:
+// A. Signature
+// B. Purpose
+// C. At least 3 examples
+// D. Function definition
+
+// Part 1:
+
+/*
+A tea shop sells its house blends of tea for $3.50 per tin of loose tea, 
+and $4.25 per box of tea bags. The shop never sells partial tins or partial 
+boxes of tea. Design a function tea_total that expects the number of 
+loose tea tins and the number of tea bag boxes, and 
+returns the total amount owed for that sale.
+*/
+
+// Part 2:
+
+/*
+A bookstore awards points to its customers based on the number of books 
+purchased each month. Design the function points which accepts the number 
+of books a customer purchased for the month, and returns the number of 
+points earned that month.
+*/
+
+// Part 3:
+
+/*
+A movie theater charges $10.00 for attending shows before 5:00 pm, 
+and $14.00 for attending later shows. In addition, the theater offers a 
+20% discount for 10 or more tickets purchased. Design get_price that 
+expects a string representing a showing type ("early" or "reg"), and 
+the number of tickets purchased, and returns the total price for 
+attending that showing type for those number of tickets. 
+If any other showing type string is given, it returns 0.0.
+*/
+
+int main()
+{
+  cout << boolalpha;
+
+  cout << "Tests for tea_total:" << endl;
+
+  cout << "Tests for points:" << endl;
+
+  cout << "Tests for get_price:" << endl;
+
+  return EXIT_SUCCESS;
+}
---
+++ lab/project-3-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Student ID 1:
+// Student ID 2:
+
+// Create a program that reads in a text file, applies
+// a shift to each character in that file, and writes
+// the shifted characters onto another text file
+
+// Test your program by encrypting and decrypting text files from:
+// https://www.gutenberg.org/browse/scores/top
+// (click a book title and download the Plain Text UTF-8 version of the book)
+
+// Use your solution from project-3-1 (as helper functions),
+// and refer to lecture notes here:
+// https://github.com/jjlumagbas/1370/tree/master/lecture/10-1
+
+int main()
+{
+
+  return EXIT_SUCCESS;
+}
