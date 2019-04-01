86097fde1521a2febba368de618094cea360b0db
JJ Lumagbas <jjlumagbas@gmail.com>
2019-04-01 09:01:43 -0500

    Add lab11-1

---
+++ lab/lab11-1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>    
+
+using namespace std;
+
+// Student ID 1:
+// Student ID 2: 
+
+
+/* For each of the below functions write:
+(IMPT: Do these steps in order)
+1. A stub
+2. At least 2 tests (different array inputs)
+3. Function body
+*/
+
+
+// A.
+// top_score: double[] int -> double
+// Returns the highest score in an array of grades
+// on an exam
+
+
+// B.
+// count_passed: double[] int -> double
+// Returns the number of grades in an array that are
+// at least 60.0 or greater
+
+
+
+// C.
+// ave_grades: double[] int -> double
+// Computes the average of all grades in an array
+
+
+
+// D.
+// ave_passed: double[] int -> double
+// Computes the averages of grades in an array
+// that are at least 60.0 or greater
+
+
+int main () {
+  cout << boolalpha;
+  
+  
+  cout << endl;
+
+  return 0;
+}
