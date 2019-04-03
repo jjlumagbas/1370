f43a11e655e9bb76a0bb5e546920d88c9e27789e
JJ Lumagbas <jjlumagbas@gmail.com>
2019-04-03 09:31:14 -0500

    Lab 11-2

---
+++ lab/lab11-2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>    
+
+using namespace std;
+
+// Student ID 1:
+// Student ID 2: 
+
+
+/* For each of the below functions write:
+(IMPT! Do these steps in order)
+1. A stub
+2. (In main) at least 2 tests (different array inputs)
+3. Full function definition (comment out the stub)
+*/
+
+
+// A.
+// is_equal: int[], int, int[], int
+// Accepts 2 integer arrays (and their lengths), and
+// returns true if the first array is equal to the second array
+// (they both have the same number of elements in order)
+
+
+// B.
+// is_subset: int[], int, int[], int
+// Accepts 2 integer arrays (and their lengths), and
+// returns true if every element in the first array
+// can be found in the second array
+
+
+// C.
+// is_contained: int[], int, int[], int
+// Accepts 2 integer arrays (and their lengths), and
+// returns true if every element in the first array
+// is present in the same order in the second array
+// Examples:
+//  {1, 2, 3} is contained in {0, 1, 2, 3, 4}
+//  {1, 2, 3} is not contained in {0, 1, 3, 2, 4}
+
+
+
+int main () {
+  cout << boolalpha;
+  
+  
+  cout << endl;
+
+  return 0;
+}
