57122d4f701e9cc1bce17b44bd8753465c6225b8
JJ Lumagbas <jjlumagbas@gmail.com>
2019-04-07 22:26:35 -0500

    Add lab11-3

---
+++ lab/lab11-3.cpp
@@ -0,0 +1,63 @@
+#include <iostream>    
+
+using namespace std;
+
+// Student ID 1:
+// Student ID 2: 
+
+
+/* For each of the below functions write:
+(IMPT! Do these steps in order)
+1. A stub (already provided!)
+2. (In main) at least 2 tests (different array inputs)
+3. Full function definition (comment out the stub)
+*/
+
+
+// A.
+// count: int[], int, int -> int
+// Counts the number of times the given int n
+// appears in an int array
+int count(int arr[], int length, int n) {
+  return 0;
+}
+
+
+
+// B.
+// is_contained_offset: int[], int, int[], int, int -> bool
+// Checks if arr1 is contained within arr2, starting from  
+// the specified index for arr2 (j_start)
+bool is_equal_offset(
+  int arr1[], int length1, 
+  int arr2[], int length2,
+  int j_start) {
+
+  return false;
+
+}
+
+
+// C.
+// is_contained: int[], int, int[], int -> bool
+// Accepts 2 integer arrays (and their lengths), and
+// returns true if every element in the first array
+// is present in the same order in the second array
+// Examples:
+//  {1, 2, 3} is contained in {0, 1, 2, 3, 4}
+//  {1, 2, 3} is not contained in {0, 1, 3, 2, 4}
+bool is_contained(int arr1[], int length1, int arr2[], int length2) {
+  return false;
+}
+
+
+
+
+int main () {
+  cout << boolalpha;
+  
+  
+  cout << endl;
+
+  return 0;
+}
