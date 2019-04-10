5425f7ec8b06c83c1e6e3b80b11b467f866acb8b
JJ Lumagbas <jjlumagbas@gmail.com>
2019-04-10 10:17:17 -0500

    Update lecture 13-2

--- lecture/13-2/2d-arrays.cpp
+++ lecture/13-2/2d-arrays.cpp
@@ -1,10 +1,8 @@
 #include <iostream>    
-
 using namespace std;
 
 const int ROW = 3;
 const int COL = 5;
-
 
 void row(int arr[ROW][COL], int r) {
   for (int j = 0; j < COL; j++) {
@@ -12,53 +10,63 @@ void row(int arr[ROW][COL], int r) {
   }
 }
 
-void col(int arr[ROW][COL], int c) {
-  for (int i = 0; i < ROW; i++) {
-    cout << arr[i][c] << " ";
-  }
-}
-
-void row_wise(int arr[ROW][COL]) {
-  for (int i = 0; i < ROW; i++) {
-    for (int j = 0; j < COL; j++) {
-      cout << arr[i][j] << " ";
-    }
-  }
-}
-
-void col_wise(int arr[ROW][COL]) {
-  for (int j = 0; j < COL; j++) {
-    for (int i = 0; i < ROW; i++) {
-      cout << arr[i][j] << " ";
-    }
-  }
-}
-
 
 int main () {
   cout << boolalpha;
 
-  int arr[ROW][COL] = {
+  int arr[5];
+
+  int arr2d[3][5] = {
     {10, 20, 30, 40, 50},
     {100, 200, 300, 400, 500},
     {1000, 2000, 3000, 4000, 5000}
   };
 
-  for (int i = 0; i < ROW; i++) {
-    for (int j = 0; j < COL; j++) {
-      cout << arr[i][j] << " ";
-    }
-    cout << endl;
+  cout << arr2d[0][4] << endl;
+  cout << arr2d[1][1] << endl;
+  cout << arr2d[2][3] << endl;
+  cout << arr2d[2][4] << endl;
+
+
+
+  // for (int i = 0; i < 3; i++) {
+  //   cout << arr2d[i][0] << " ";
+  // }
+  // cout << endl;
+
+  // for (int j = 0; j < 5; j++) {
+  //   cout << arr2d[0][j] << " ";
+  // }
+
+  // for (int j = 0; j < 5; j++) {
+  //   cout << arr2d[1][j] << " ";
+  // }
+  
+  // for (int j = 0; j < 5; j++) {
+  //   cout << arr2d[2][j] << " ";
+  // } 
+
+  cout << endl;
+
+  int el = 0;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 5; j++) {
+       el = arr2d[i][j];
+       cout << el << " ";
+    } 
   }
 
-  row(arr, 2);
-  cout << endl;
-  col(arr, 2);
   cout << endl;
 
-  row_wise(arr);
+  for (int j = 0; j < 5; j++) {
+    for (int i = 0; i < 3; i++) {
+       el = arr2d[i][j];
+       cout << el << " ";
+    } 
+  }
   cout << endl;
-  col_wise(arr);
+
+  row(arr2d, 2);
 
   return 0;
 }
