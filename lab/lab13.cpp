e430afe4dc93f32be420ca585c87c3df4e3b6d15
JJ Lumagbas <jjlumagbas@gmail.com>
2019-04-16 21:51:09 -0500

    Add lab13

---
+++ lab/lab13.cpp
@@ -0,0 +1,28 @@
+#include <iostream>    
+
+using namespace std;
+
+// Student ID 1:
+// Student ID 2: 
+
+
+
+// Write a function called shorty that takes an array of strings (and the length
+// of the array) and returns the shortest string (if there is more than one
+// shortest string, any one of them will do).
+
+
+// Write a function called ceiling that takes an array of doubles (and the
+// length of the array) and another double max and returns the average of the
+// elements that are greater than max.
+
+
+
+int main () {
+  cout << boolalpha;
+
+  
+  cout << endl;
+
+  return 0;
+}
